c0c83a49ec712fb1e062073c1a6bf2f47916010c
dinamyc <dinamycdinamyc@gmail.com>
2025-02-21 17:37:06 +0330

    feat(sway): integrate rofi and powermenu

--- sway/script/powermenu.sh
+++ sway/script/powermenu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # https://gist.github.com/mxdevmanuel/a2229d427b39a9e40f2198979caa40c1
 
-op=$( echo -e " Poweroff\n Reboot\n WinBoot\n Suspend\n Lock\n Logout" | wofi -i --dmenu | awk '{print tolower($2)}' )
+op=$( echo -e " Poweroff\n Reboot\n WinBoot\n Suspend\n Lock\n Logout" | rofi -dmenu -i | awk '{print tolower($2)}' )
 
 case $op in 
   poweroff)
