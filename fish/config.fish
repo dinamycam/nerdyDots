3215d358b78645198caf6946ab355aaa9417167a
dinamyc <dinamycdinamyc@gmail.com>
2025-04-18 00:26:13 +0330

    fix(fish): add EDITOR, configure ssh-agent from systemd user service, comment them properly

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 		starship init fish | source
+        batman --export-env | source
 		fish_vi_key_bindings
 end
-batman --export-env | source
+set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+# replace cat with bat
 alias cat bat
+
+# editor and visual for sudoedit and visudo
+set -x EDITOR "nvim"
+set -x VISUAL "nvim"
+
