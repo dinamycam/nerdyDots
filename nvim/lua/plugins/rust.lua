7579c9b789996e7b5eda94d5504567e8febe375f
dinamyc <dinamycdinamyc@gmail.com>
2025-07-19 01:24:19 +0330

    feat(nvim): rustacean sane default settings

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -1,5 +1,16 @@
 return {
-	"mrcjkb/rustaceanvim",
-	version = "^6", -- Recommended
-	lazy = false, -- This plugin is already lazy
+    "mrcjkb/rustaceanvim",
+    version = "^6", -- Recommended
+    lazy = false,   -- This plugin is already lazy
+    -- config = function()
+    --     settings = {
+    --         ['rust-analyzer'] = {
+    --             server = {
+    --                 trace = {
+    --                     server = { 'verbose' },
+    --                 },
+    --             },
+    --         },
+    --     }
+    -- end,
 }
