f966d3896e879662570cc00266475958035087ac
dinamyc <dinamycdinamyc@gmail.com>
2025-07-19 13:08:16 +0330

    feat(nvim): steal ideas from minimalist nvim (https://github.com/radleylewis/nvim-lite/tree/master)

--- nvim/lua/user/settings.lua
+++ nvim/lua/user/settings.lua
@@ -8,17 +8,77 @@ vim.opt.shiftwidth = 4 -- Number of spaces for indentation
 vim.opt.tabstop = 4 -- Number of spaces a tab counts for
 vim.opt.smartindent = true -- Smart indentation
 vim.opt.wrap = false -- Disable line wrapping
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.hlsearch = false -- Highlight search results
-vim.opt.incsearch = true -- Incremental search
--- vim.opt.termguicolors = true         -- Enable 24-bit RGB colors (heh, already enabled)
-vim.opt.scrolloff = 8 -- Keep 8 lines above/below the cursor
+vim.opt.incsearch = false -- Incremental search
+vim.opt.termguicolors = true -- Enable 24-bit RGB colors (heh, already enabled)
 -- vim.opt.signcolumn = "yes"           -- Always show the sign column
+vim.opt.colorcolumn = "100"
+vim.opt.scrolloff = 8 -- Keep 8 lines above/below the cursor
+
+--file handling
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.undofile = true
+local undodir = vim.fn.expand("~/.vim/undodir")
+vim.opt.undodir = undodir
+vim.opt.autoread = true
+vim.opt.autowrite = false
+vim.opt.winblend = 10
+
+-- Buffer navigation
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "goto Next buffer" })
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "goto previous buffer" })
+
+-- Splitting and Resizing and Closing
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window Vertically" })
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window Horizontally" })
+
+vim.keymap.set("n", "<leader>bc", ":close<CR>", { desc = "Close current buffer" })
+
+vim.keymap.set("n", "<C-k>", ":resize +2<CR>", { desc = "increase window height" })
+vim.keymap.set("n", "<C-j>", ":resize -2<CR>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-h>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-l>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+
+-- Basic autocommands
+local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup,
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = augroup,
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
+
+-- Auto-resize splits when window is resized
+vim.api.nvim_create_autocmd("VimResized", {
+	group = augroup,
+	callback = function()
+		vim.cmd("tabdo wincmd =")
+	end,
+})
 
 -- neovide setup
 if vim.g.neovide then
 	-- vim.o.guifont = "Fira Code:h7"
-	vim.opt.linespace = 2
-	-- vim.g.neovide_scale_factor = 0.5
+	vim.opt.linespace = 0
+	-- vim.g.neovide_scale_factor = 0.8
 
 	vim.g.neovide_floating_blur_amount_x = 2.0
 	vim.g.neovide_floating_blur_amount_y = 2.0
@@ -116,3 +176,8 @@ require("telescope").load_extension("notify")
 require("telescope").load_extension("project")
 -- not configured
 -- vim.lsp.enable("pyright")
+
+-- Create undo directory if it doesn't exist
+if vim.fn.isdirectory(undodir) == 0 then
+	vim.fn.mkdir(undodir, "p")
+end
